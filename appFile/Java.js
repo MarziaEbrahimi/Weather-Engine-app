4c15afb2037fb9361b2f6a19df6f91f0f66b1377
Marzia <eb.marzia@gmail.com>
2023-03-06 17:50:53 -0500

    Starting to code on Java

--- appFile/Java.js
+++ appFile/Java.js
@@ -1,1 +1,16 @@
+function Temprature(response) {
+  let degreeElement = document.querySelector("#show-degree");
+  degreeElement.innerHTML = Math.round(response.data.main.temp);
+  let cityName = document.querySelector("h6");
+  cityName.innerHTML = response.data.name;
+  let countryName = document.querySelector("#country");
+  countryName.innerHTML = response.data.sys.country;
 
+  console.log(response.data);
+}
+
+let apiKey = "9666cb098baebeb992cfd789750f6f47";
+let cityName = "Philadelphia";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(Temprature);
